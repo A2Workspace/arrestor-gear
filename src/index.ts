945bc7851745f685c412cffda6c679478d0b0b84
Shisha <shishatw225@gmail.com>
2022-08-16 07:05:01 +0800

    Build

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ export default function arrestorGear(promise: Promise<any>): ArrestorGear {
   return new ArrestorGear(promise);
 }
 
-export * from './types';
-
 export {
   ArrestorGear,
   ValidationMessageBag,
