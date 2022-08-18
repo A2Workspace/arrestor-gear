9db1a0f0fac2f870757f0ee5c8bbe289c2ded06c
Shisha <shishatw225@gmail.com>
2022-08-18 13:15:32 +0800

    分離 simple factory function and core instances

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
 import ArrestorGear from './core/ArrestorGear';
-import { matchHttpError, matchHttpStatusCode, matchHttpValidationError } from './core/utils';
-import { captureAxiosError, captureStatusCode, captureValidationError } from './core/capturers';
-import ValidationMessageBag from './core/ValidationMessageBag';
 
 export default function arrestorGear(promise: Promise<any>): ArrestorGear {
   return new ArrestorGear(promise);
 }
-
-export {
-  ArrestorGear,
-  ValidationMessageBag,
-  matchHttpError,
-  matchHttpStatusCode,
-  matchHttpValidationError,
-  captureAxiosError,
-  captureStatusCode,
-  captureValidationError,
-};
